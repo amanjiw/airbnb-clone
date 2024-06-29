61d576b01db901fd5b82e34525e160931c592404
amanji <ghaderi.amanj@gmail.com>
2024-06-29 20:59:50 +0330

    feat:implement navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import DesktopLogo from "@/public/airbnb-desktop.png";
+import MobileLogo from "@/public/airbnb-mobile.webp";
 
 const Navbar = () => {
-	return <div></div>;
+	return (
+		<div className="w-full border-b">
+			<div className="flex items-center justify-between mx-auto px-5 lg:px-10 py-5">
+				<Link href="/">
+					<Image
+						src={DesktopLogo}
+						alt="desktop logo"
+						className="w-32 hidden lg:block"
+					/>
+					<Image
+						src={MobileLogo}
+						alt="mobile logo"
+						className="block lg:hidden w-12"
+					/>
+				</Link>
+				<div className="rounded-full border px-5 py-2">
+					<h1>hello from serch</h1>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
